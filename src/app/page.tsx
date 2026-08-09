7b193e09a62d5577696490b1c94466d12099bb3d
spss789 <mehfilemasti5@gmail.com>
2026-08-09 14:41:48 +0530

    Fix nextjs dynamic route rendering for vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import prisma from "@/lib/prisma";
